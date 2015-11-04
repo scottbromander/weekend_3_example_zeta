e12fdacce34df29d528ccc027ea7de40b7ff8ae1
Scott Bromander <scott@primeacademy.io>
2015-11-03 20:37:57 -0600

    initial commit

--- server/app.js
+++ server/app.js
@@ -5,15 +5,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var math = require('./routes/math');
-var multi = require('./routes/multi');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
 
 
-app.use('/math', math);
-app.use('/math/multi', multi);
+
 app.use('/', index);
 
 app.listen(app.get('port'), function(){
